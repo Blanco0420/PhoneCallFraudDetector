ff1908dd5a85872fd8f7ff81edbdd261fc441798
Blanco0420 <sachajamesterry@gmail.com>
2025-06-25 16:41:41 +0900

    implemented japanese tokenizing

--- config/config.go
+++ config/config.go
@@ -1,26 +1,22 @@
 package config
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/fatih/color"
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
 )
-
-func Warning(message string) {
-	yellow := color.New(color.FgYellow).SprintFunc()
-	fmt.Println(yellow("[WARN] %s", message))
-}
 
 func LoadEnv() {
 	env := os.Getenv("APP_ENV")
 
 	if env == "development" {
 		if err := godotenv.Load(); err != nil {
-			Warning("Failed to load .env file. Continuing without.")
+			log.Warn().Msg("Failed to load .env file. Continuing without.")
 		}
 	}
+
+	initLevenshtein()
 }
 
 func GetEnvVariable(variableToCheck string) (string, bool) {
